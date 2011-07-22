497a7d19e300da478cb461c5d73f62e2b13bd972
neil <neil@neil.localdomain>
2011-07-22 17:10:48 +0100

    yet more syntax fixing

--- ow_extensions/csvimport/scripts/csvimport.js
+++ ow_extensions/csvimport/scripts/csvimport.js
@@ -770,7 +770,7 @@ ok now the fun bit, lets parse the dimensions_raw object
 			} 
 			dimensions[ylcs + "structure" + input_measure_name + dim.capitalize()] = {};
 			dimensions[ylcs + "structure" + input_measure_name + dim.capitalize()][rdfs + "subClassOf"] = [make_rdf_object(input_measure_name, ylcs)];
-			dimensions[ylcs + "structure" = input_measure_name + dim.capitalize()][qb + "component"] =  [{"type": "bnode", "value" : "_:" + dim}];
+			dimensions[ylcs + "structure" + input_measure_name + dim.capitalize()][qb + "component"] =  [{"type": "bnode", "value" : "_:" + dim}];
 			dimensions[ylcs + dataset_name + dim.capitalize()] = { };
 			//dimensions[ylcs + "datasetProportionOfSample" + dim.capitalize()] = {"http://www.w3.org/1999/02/22-rdf-syntax-ns#type" : [make_rdf_object("DataSet", qb)]};
 			dimensions[ylcs + dataset_name + dim.capitalize()][qb + "structure"] = [make_rdf_object("structure" + input_measure_name + dim.capitalize(), ylcs)];
