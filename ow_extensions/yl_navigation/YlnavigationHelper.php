d5579babe664992b7fc459474510e8ce828e9328
= <=>
2011-10-17 11:19:24 +0100

    adding new box on left for counrty nav

---
+++ ow_extensions/yl_navigation/YlnavigationHelper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Helpers
+ * @category   OntoWiki
+ * @package    OntoWiki_extensions_components_yl_navigation
+ */
+class YlnavigationHelper extends OntoWiki_Component_Helper
+{
+    
+    public static function canWriteHelperFunctionsHere(){
+
+    }
+        
+}
+
